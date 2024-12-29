74abea10ea8673b8c933659b62e74ffafa1c9c1d
Avinash <avimalhotra505@gmail.com>
2024-12-29 12:23:07 +0530

    Angular class 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CourseComponent } from "./course/course.component";
+import { TrainerComponent } from './trainer/trainer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CourseComponent, TrainerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ngApp';
+  title = 'Angular';
+  version=19;
 }
---
+++ src/app/trainer/trainer.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trainer',
+  imports: [],
+  templateUrl: './trainer.component.html',
+  styleUrl: './trainer.component.css'
+})
+export class TrainerComponent {
+    trainer="Avinash";
+    subject="Mean Stack";
+    since=2016;
+}
