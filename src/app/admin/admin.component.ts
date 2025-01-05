7ccfd685bd17c7e8cf699402fe3e731bbab947d4
Avinash <avimalhotra505@gmail.com>
2025-01-05 12:37:33 +0530

    data binding

---
+++ src/app/admin/admin.component.ts
@@ -0,0 +1,18 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-admin',
+    imports:[],
+    templateUrl:'./admin.component.html',
+    styleUrl:'./admin.component.css',
+
+    // template:`<h1>Hello Admin</h1><p>Admin is online</p>`,
+    // styleUrls:['./admin.component.css']
+    // styles:['h1{ color:red }']
+    // styleUrls:['./admin.component.css']
+})
+
+
+export class AdminComponent{
+    title="Admin"
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,41 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { CourseComponent } from "./course/course.component";
 import { TrainerComponent } from './trainer/trainer.component';
+import { AdminComponent } from './admin/admin.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CourseComponent, TrainerComponent],
+  imports: [CourseComponent, TrainerComponent, AdminComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'Angular';
   version=19;
+  author={name:"Avinash", company:"Tech Altum"};
+  
+  arr=[1,3,2, 10, 21 ];
+  car={ name:"Swift", power:82, torque:112 };
+  
+  numSrt=function(a:number,b:number){return a-b};
+  reverseString(x:string){return x.split("").reverse().join("");}
+  reverseWords(x:string){return x.split(" ").reverse().join(" ");}
+
+
+  jack={image:"jack.webp", alt:"Jack Efron", width:70, height:70};
+
+  disabled=false;
+  p="para";
+  p1="color:purple";
+
+  h2="lorem ipsum";
+  upperCase(){ this.h2=this.h2.toUpperCase() }
+
+  counter=0;
+  inc(){ this.counter=this.counter+1 }
+  dec(){ this.counter=this.counter-1 }
+
+
 }
