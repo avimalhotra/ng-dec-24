5ae173be7fcfce2401a3baace81c268108636e52
Avinash <avimalhotra505@gmail.com>
2025-01-19 13:07:30 +0530

    If Else and loops

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,42 @@
 import { Component } from '@angular/core';
 import { CarsComponent } from './cars/cars.component';
-import {FormsModule} from '@angular/forms';
-import { NgClass, NgOptimizedImage } from '@angular/common';
+import { CommonModule } from '@angular/common';
+
 
 
 @Component({
   selector: 'app-root',
-  imports: [ CarsComponent, FormsModule, NgClass, NgOptimizedImage ],
+  imports: [ CarsComponent, CommonModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   preserveWhitespaces:true
 })
 
 export class AppComponent {
-  title = 'Angular';  
-  age=0;
+    title = 'Angular';  
 
-  x="active";
+    swift={ name:"Swift", fuel:"Petrol", hybrid:false, awd: false }
+    grandvitara={ name:"Grand Vitara", fuel:"Petrol", hybrid:true, awd: true }
 
-  img1={ src:"jack.webp", alt:"Jack Efron"};
-  
+    score=58;
+
+    x=null;
+
+    
+    months=["Jan","Feb","Mar","Apr"];
+    phone={name:"Iphone 15", brand:"Apple", ram:6, storage:128};
+
+    cars=[
+      {"name": "swift", "type": "hatchback", "price":810000},
+      {"name": "dzire", "type": "hatchback", "price":880000},
+      {"name": "ciaz", "type": "sedan", "price":1000000},
+      {"name": "baleno", "type": "hatchback", "price":850000},
+      {"name": "brezza", "type": "suv", "price":1400000},
+      {"name": "fronx", "type": "hatchback", "price":1000000},
+      {"name": "jimny", "type": "suv", "price":1300000},
+      {"name": "grand vitara", "type": "suv", "price":1990000}
+    ];
+    
+
+
 }
