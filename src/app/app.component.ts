6fef6b914206bc9ab802aa5ed88a41ea56255c5c
Avinash <avimalhotra505@gmail.com>
2025-01-25 12:24:37 +0530

    ngSwitch, ngStyle and @let

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,42 +1,21 @@
 import { Component } from '@angular/core';
-import { CarsComponent } from './cars/cars.component';
+// import { CarsComponent } from './cars/cars.component';
 import { CommonModule } from '@angular/common';
-
+import { CourseComponent } from './course/course.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [ CarsComponent, CommonModule ],
+  imports: [ CommonModule, CourseComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   preserveWhitespaces:true
 })
 
 export class AppComponent {
-    title = 'Angular';  
+  title = 'Angular';  
+  month=1;
+  // css={color:'red','text-align':'center'};
 
-    swift={ name:"Swift", fuel:"Petrol", hybrid:false, awd: false }
-    grandvitara={ name:"Grand Vitara", fuel:"Petrol", hybrid:true, awd: true }
-
-    score=58;
-
-    x=null;
-
-    
-    months=["Jan","Feb","Mar","Apr"];
-    phone={name:"Iphone 15", brand:"Apple", ram:6, storage:128};
-
-    cars=[
-      {"name": "swift", "type": "hatchback", "price":810000},
-      {"name": "dzire", "type": "hatchback", "price":880000},
-      {"name": "ciaz", "type": "sedan", "price":1000000},
-      {"name": "baleno", "type": "hatchback", "price":850000},
-      {"name": "brezza", "type": "suv", "price":1400000},
-      {"name": "fronx", "type": "hatchback", "price":1000000},
-      {"name": "jimny", "type": "suv", "price":1300000},
-      {"name": "grand vitara", "type": "suv", "price":1990000}
-    ];
-    
-
-
+  
 }
