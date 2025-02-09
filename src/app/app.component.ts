f61eaa3fdd938c381a8d3992e5d6f049b1c5b6e7
Avinash <avimalhotra505@gmail.com>
2025-02-09 13:07:27 +0530

    angular forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,24 +3,26 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {FormsModule} from '@angular/forms';
 // import { CourseComponent } from './course/course.component';
-import { ReversePipe } from './reverse.pipe';
+// import { ReversePipe } from './reverse.pipe';
+import { Login } from './login';
 
 
 @Component({
   selector: 'app-root',
-  imports: [ CommonModule, FormsModule , ReversePipe ],
+  imports: [ CommonModule, FormsModule  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   preserveWhitespaces:true
 })
 
 export class AppComponent {
-  title = 'Angular';  
 
-  user={name:"lorem", city:"noida"};
+  title = 'angular';
+  model = new Login("","","","","",false);                  // initial values
 
-  date = new Date();
+  onSubmit(data:any){
+    console.log( data.value );
+    
+  }
 
-  word="Lorem";
-  
 }
---
+++ src/app/login.ts
@@ -0,0 +1,3 @@
+export class Login{
+    constructor(public name:string, public email: string, public pass: string, public gender: string, public city : string, public terms: boolean){ }
+}
