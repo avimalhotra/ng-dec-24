2662a05707ca52c2db5713fe3942d7eea3ec1dac
Avinash <avimalhotra505@gmail.com>
2025-03-01 12:28:38 +0530

    Reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 // import { CarsComponent } from './cars/cars.component';
 import { CommonModule } from '@angular/common';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, Validators} from '@angular/forms';
 // import { CourseComponent } from './course/course.component';
 // import { ReversePipe } from './reverse.pipe';
-import { Login } from './login';
+// import { Login } from './login';
+import { ReactiveFormsModule } from '@angular/forms';
+import {  Validator, FormControl, FormArray, FormGroup } from '@angular/forms';
+// import { FormBuilder } from '@angular/forms';
+
 
 
 @Component({
   selector: 'app-root',
-  imports: [ CommonModule, FormsModule  ],
+  imports: [ CommonModule, FormsModule, ReactiveFormsModule  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   preserveWhitespaces:true
@@ -18,11 +22,31 @@ import { Login } from './login';
 export class AppComponent {
 
   title = 'angular';
-  model = new Login("","","","","",false);                  // initial values
 
-  onSubmit(data:any){
-    console.log( data.value );
-    
-  }
+  /* model = new Login("","","","","",false);                  
+  onSubmit(data:any){console.log( data.value );} */
+
+  // email=new FormControl('');
+  // pass=new FormControl('');
+  // age=new FormControl(0);
+
+  loginform=new FormGroup({
+    email:new FormControl("",[Validators.required, Validators.minLength(6)]),
+    pass:new FormControl("",[Validators.required,Validators.minLength(8)], ),
+    age:new FormControl(0,[Validators.required, Validators.min(25), Validators.max(75)]),
+  });
+
+  loginUser(){
+    console.log(this.loginform.status);
+    console.log(this.loginform.value);
+    console.log(this.loginform.valid);
+  };
+
+
+  setVal(){this.loginform.setValue({email:"mail@domain.com",pass:"12345678",age:25})}
+  patchVal(){ this.loginform.patchValue({email:"new@mail.com"})};
+
+
+
 
 }
